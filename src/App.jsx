50d5bffb523c2fb520ab8b2a8d4e26983efde4e9
Kritpavin <kitpavin@gmail.com>
2021-08-30 20:16:18 +0700

    get data from firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import Note from "./components/Note";
-import {addData, getData} from './controllers/DBcontrollerTest'
+import { addData, getData } from './controllers/DBcontrollerTest'
 import { Center, Container } from "./styledCoponents/MainStyle";
 
 function App() {
   const [text, setText] = useState('')
-  const [test, setTest] = useState('')
-  useEffect(() => {
+  const [test, setTest] = useState([])
 
+  const get = () => {
+    
+    getData()
+      .then((querySnapshot) => {
+        const tmpTest = []
+        querySnapshot.forEach((doc) => {
+          console.log(doc.id, " => ", doc.data());
+          tmpTest.push(doc.data().note)
+        });
+        setTest(tmpTest)
+      })
+  }
+
+  const saveData = () => {
+    addData(text);
+    setText("")
+  }
+
+  useEffect(() => {
+    get()
   }, []);
 
   return (
     <Container>
       <Center>
-        <Note md={'- Revenue was off the chart \n - Profits were higher than ever.'} />
-
+        {/* <Note md={'- Revenue was off the chart \n - Profits were higher than ever.'} /> */}
+        {
+          test.map((note) => <Note md={note} />)
+        }
+        ----------------
         <Note md={text} />
-        <textarea type="text-area" name="name" onChange={(e) => setText(e.target.value)}/>
-        <button onClick={() => {addData(text); setText("")}}>save</button>
+        <textarea value={text} type="text-area" name="name" onChange={(e) => setText(e.target.value)} />
+        <button onClick={() => saveData()}>save</button>
       </Center>
     </Container>
   );
--- src/controllers/DBcontrollerTest.js
+++ src/controllers/DBcontrollerTest.js
@@ -1,5 +1,7 @@
 import { db, firebase } from '../configs/FirebaseConfig'
 
+const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+
 export const setData = () => {
     // set() have to use own ID
     db.collection("cities").doc("compilcate").set({
@@ -22,7 +24,8 @@ export const setData = () => {
 export const addData = (text) => {
     // add() can genarate ID
     db.collection("testMD").add({
-        note: text
+        note: text,
+        createAt: timestamp
     })
         .then((docRef) => {
             console.log("Document written with ID: ", docRef.id);
@@ -33,39 +36,13 @@ export const addData = (text) => {
 }
 
 export const readRealTime = () => {
-    db.collection("cities").doc("compilcate")
+    db.collection("cities")
         .onSnapshot((doc) => {
             console.log("Current data: ", doc.data());
         });
 }
 
-export const readColloction = () => {
-    // read all doc in collection
-    db.collection("testMD")
-        .get()
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                console.log(doc.id, " => ", doc.data());
-            });
-        })
-        .catch((error) => {
-            console.log("Error getting documents: ", error);
-        });
-}
-
 export const getData = () => {
-    var docRef = db.collection("testMD").doc("Bai1BPndMpdwVO5Ktw1y");
-    docRef.get().then((doc) => {
-        if (doc.exists) {
-            console.log("Document data:", doc.data());
-            return "doc.data()"
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-            return ""
-        }
-    }).catch((error) => {
-        console.log("Error getting document:", error);
-    });
+    const doc = db.collection("testMD").orderBy('createAt').get();
+    return doc;
 }
