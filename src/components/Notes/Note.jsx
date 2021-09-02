79ae1138b31ceb3b4ccf19f560a3013b69333658
Kritpavin <kitpavin@gmail.com>
2021-09-02 18:49:43 +0700

    motive and expand note

--- src/components/Notes/Note.jsx
+++ src/components/Notes/Note.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { renderMD } from "../../controllers/MDcontroller";
 import { NoteCard } from "../../styledCoponents/NoteStyle";
+import { motion } from "framer-motion";
 
 export default function Note(props) {
+  const constraintsRef = useRef(null);
+  const [isExpandMode, setisExpandMode] = useState(false)
+
   return (
     <>
-      <NoteCard onClick={() => console.log(props.noteObj)}>
-        {renderMD(props.noteObj.note)}
-      </NoteCard>
+      <motion.div ref={constraintsRef}>
+        <motion.div drag dragConstraints={constraintsRef}>
+          {
+            isExpandMode ?
+              <NoteCard onClick={() => setisExpandMode(!isExpandMode)}>
+                <p stype>{props.noteObj.title}</p>
+                {renderMD(props.noteObj.note)}
+              </NoteCard>
+              :
+              <NoteCard onClick={() => setisExpandMode(!isExpandMode)}>
+                <p stype>{props.noteObj.title}</p>
+              </NoteCard>
+
+          }
+        </motion.div>
+      </motion.div>
     </>
   );
 }
