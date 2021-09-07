ea5c3fbad3f225fa931e8c3dee71ddea95f45ede
Kritpavin <kitpavin@gmail.com>
2021-09-07 20:55:48 +0700

    finish note feature

--- src/components/Notes/Note.jsx
+++ src/components/Notes/Note.jsx
@@ -1,15 +1,60 @@
 import React, { useState, useRef } from "react";
 import { renderMD } from "../../controllers/MDcontroller";
 import { NoteCard, ShowMoreBTN, Title, Box, DeleteNoteBTN } from "../../styledCoponents/NoteStyle";
+import { Button } from '../../styledCoponents/MainStyle'
 import { motion } from "framer-motion";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 import { AiOutlineDelete } from 'react-icons/ai'
 import InlineDialog from '@atlaskit/inline-dialog';
+import { deleteNote } from '../../controllers/DBcontrollerTest'
 
 export default function Note(props) {
   const constraintsRef = useRef(null);
   const [isExpandMode, setisExpandMode] = useState(false);
   const [dialogOpen, setDialogOpen] = useState(false)
+
+  const deleteNoteFunc = (docId) => {
+    deleteNote(docId)
+    props.deleteNoteTriggle(docId)
+    setDialogOpen(false)
+  }
+
+  const deleteNoteDialog = (docId) => {
+    return <InlineDialog
+      onClose={() => setDialogOpen(false)}
+      placement="right"
+      content={
+        <div>
+          <Box>
+            <div style={{ marginRight: "11px", paddingTop: "5px" }}>Delete ?</div>
+            <Button
+              onClick={() => deleteNoteFunc(docId)}
+              style={{ marginRight: "4px", marginTop: "-5px" }}
+              bgColor="#E74C3C "
+              hoverColor="#E74C3C "
+              textColor="white"
+            >delete
+                </Button>
+            <Button
+              onClick={() => setDialogOpen(false)}
+              style={{ marginTop: "-5px" }}
+              bgColor="#CACFD2"
+              hoverColor="#CACFD2"
+              textColor="black"
+            >cancel
+                </Button>
+          </Box>
+        </div>
+      }
+      isOpen={dialogOpen}
+    >
+      <DeleteNoteBTN onClick={() => setDialogOpen(!dialogOpen)}>
+        <AiOutlineDelete />
+      </DeleteNoteBTN>
+    </InlineDialog>
+
+  }
+
 
   return (
     <div ref={constraintsRef}>
@@ -22,18 +67,7 @@ export default function Note(props) {
             <Title>
               <p>{props.noteObj.title}</p>
               <Box>
-                <InlineDialog
-                  onClose={() => setDialogOpen(false)}
-                  content={<div>
-                    <p>Hello!</p>
-                  </div>}
-                  isOpen={dialogOpen}
-                >
-                  <DeleteNoteBTN onClick={() => setDialogOpen(!dialogOpen)}>
-                    <AiOutlineDelete />
-                  </DeleteNoteBTN>
-                </InlineDialog>
-
+                {deleteNoteDialog(props.noteObj.id)}
                 <ShowMoreBTN onClick={() => setisExpandMode(!isExpandMode)}>
                   <MdKeyboardArrowUp />
                 </ShowMoreBTN>
@@ -46,16 +80,12 @@ export default function Note(props) {
       ) : (
           <motion.div drag dragConstraints={constraintsRef}>
             <motion.li whileHover={{ scale: 1.03 }}>
-              <NoteCard isShowMore={isExpandMode} onClick={() => { console.log(props.noteObj) }}>
+              <NoteCard isShowMore={isExpandMode}>
                 <div style={{ display: "flex", justifyContent: "space-between" }}>
                   <p style={{ fontWeight: "500" }}>{props.noteObj.title}</p>
                   <p></p>
                   <Box>
-                    <DeleteNoteBTN>
-                      <AiOutlineDelete />
-                    </DeleteNoteBTN>
-
-
+                    {deleteNoteDialog(props.noteObj.id)}
                     <ShowMoreBTN onClick={() => setisExpandMode(!isExpandMode)}>
                       <MdKeyboardArrowDown style={{ marginTop: "3px" }} />
                     </ShowMoreBTN>
--- src/components/Notes/NoteSection.jsx
+++ src/components/Notes/NoteSection.jsx
@@ -16,8 +16,9 @@ function NoteSection() {
   const timestamp = firebase.firestore.FieldValue.serverTimestamp();
   const [note, setNote] = useState("");
   const [noteTitle, setNoteTitle] = useState("");
-  const [test, setTest] = useState([]);
+  const [noteList, setNoteList] = useState([]);
   const [isShowAddNoteForm, setIsShowAddNoteForm] = useState(false);
+  const [isShowHRSeperate, setisShowHRSeperate] = useState(true)
 
   useEffect(() => {
     get();
@@ -26,6 +27,8 @@ function NoteSection() {
   const get = () => {
     getNotes().then((querySnapshot) => {
       const tmpNoteList = [];
+      setIsShowAddNoteForm(querySnapshot.docs.length === 0)
+      setisShowHRSeperate(querySnapshot.docs.length !== 0)
       querySnapshot.forEach((doc) => {
         let tmp = {
           id: doc.id,
@@ -35,22 +38,24 @@ function NoteSection() {
         };
         tmpNoteList.push(tmp);
       });
-      setTest(tmpNoteList.reverse());
+      setNoteList(tmpNoteList.reverse());
     });
   };
 
   const saveData = () => {
+    // setIsShowAddNoteForm(false)
     if (noteTitle !== "" && note !== "") {
       addNote(noteTitle, note)
         .then((docRef) => {
-          setTest([
+          setisShowHRSeperate(true)
+          setNoteList([
             {
               id: docRef.id,
               title: noteTitle,
               note: note,
               createAt: timestamp,
             },
-            ...test,
+            ...noteList,
           ]);
         })
         .catch((error) => {
@@ -70,14 +75,23 @@ function NoteSection() {
     setNote("")
   }
 
+  const deleteNoteTriggle = (docId) => {
+    var tmpCurrentNote = noteList.filter(item => { return item.id !== docId })
+    setNoteList(tmpCurrentNote)
+    console.log(tmpCurrentNote.length);
+    if (tmpCurrentNote.length === 0) {
+      setIsShowAddNoteForm(true)
+      setisShowHRSeperate(false)
+    }
+
+
+  }
+
 
   return (
-    <>
-      {/* <Container>
-        <Center> */}
+    <div >
       <Box>
-        {/* <div>note</div> */}
-        <div></div>
+        <div style={{marginLeft: "25px", marginTop: "6px", fontSize: "22px", fontWeight: "600"}}>My note</div>
 
         <div ref={constraintsRef}>
           <motion.div drag dragConstraints={constraintsRef}>
@@ -101,74 +115,73 @@ function NoteSection() {
                   </Button>
                 )
             }
-
           </motion.div>
         </div>
       </Box>
+
       <NotePreview md={note} title={noteTitle} />
+      <div style={{ width: "416px" }}>
+        {
+          isShowAddNoteForm &&
+          <div style={{ alignItems: "center", paddingRight: "13px", paddingLeft: "13px" }}>
+            <div>
+              <Textfield
+                style={{ fontWeight: "600" }}
+                value={noteTitle}
+                onChange={(e) => setNoteTitle(e.target.value)}
+                type="text"
+                placeholder="Note title"
+                maxLength={40} />
+            </div>
+            <div>
+              <TextArea
+                style={{ height: "100px", marginTop: "5px" }}
+                value={note}
+                onChange={(e) => setNote(e.target.value)}
+                resize="vertical"
+                placeholder="Note detail"
+                name="area" />
+            </div>
+            {
+              (noteTitle !== "" || note !== "") &&
+              <Box>
+                <p></p>
+                <SaveBtnContainer>
+                  <Button
+                    onClick={() => { setNoteTitle(""); setNote("") }}
+                    style={{ marginBottom: "10px", marginRight: "13px" }}
+                    bgColor="#EBDEF0 "
+                    hoverColor="#EBDEF0 "
+                    textColor="black"
+                  >
+                    clear form
+                </Button>
+                  <Button
+                    disabled={noteTitle === "" || note === ""}
+                    onClick={() => saveData()}
+                    style={{ marginBottom: "10px", marginRight: "13px" }}
+                    bgColor="#76D7C4 "
+                    hoverColor="#76D7C4 "
+                    textColor="white"
+                  >
+                    save
+                </Button>
+                </SaveBtnContainer>
+              </Box>
+            }
+            {
+              isShowHRSeperate && <hr />
+            }
+          </div>
+        }
+      </div>
 
       {
-        isShowAddNoteForm &&
-        <div style={{ alignItems: "center", paddingRight: "13px", paddingLeft: "13px" }}>
-          <div>
-            <Textfield
-              style={{ fontWeight: "600" }}
-              value={noteTitle}
-              onChange={(e) => setNoteTitle(e.target.value)}
-              type="text"
-              placeholder="Note title"
-              maxLength={40} />
-          </div>
-          <div>
-            <TextArea
-              style={{ height: "100px", marginTop: "5px" }}
-              value={note}
-              onChange={(e) => setNote(e.target.value)}
-              resize="vertical"
-              placeholder="Note detail"
-              name="area" />
-          </div>
-          {
-            (noteTitle !== "" || note !== "") &&
-            <Box>
-              <p></p>
-
-              <SaveBtnContainer>
-                <Button
-                  onClick={() => { setNoteTitle(""); setNote("") }}
-                  style={{ marginBottom: "10px", marginRight: "13px" }}
-                  bgColor="#EBDEF0 "
-                  hoverColor="#EBDEF0 "
-                  textColor="black"
-                >clear form
-                </Button>
-
-                <Button
-                  onClick={() => saveData()}
-                  style={{ marginBottom: "10px", marginRight: "13px" }}
-                  bgColor="#76D7C4 "
-                  hoverColor="#76D7C4 "
-                  textColor="black"
-                >save
-                </Button>
-
-              </SaveBtnContainer>
-
-
-            </Box>
-          }
-          <hr />
-        </div>
+        noteList.map((noteObj) => (
+          <Note noteObj={noteObj} deleteNoteTriggle={deleteNoteTriggle} />
+        ))
       }
-
-
-
-      {test.map((noteObj) => (
-        <Note noteObj={noteObj} />
-      ))}
-      {/* </Center>
-      </Container> */}
-    </>
+    </div>
   );
 }
 
